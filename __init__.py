a25a9a3b2644f0dbf381da0e7d947e4a1a8f4a78
JosefLeininger03 <jolei7444@hvl.no>
2025-11-12 11:48:00 +0100

    Change database URI to hardcoded values
    Updated database URI to use hardcoded credentials.

--- __init__.py
+++ __init__.py
@@ -8,6 +8,7 @@ application = Flask(__name__)
 
 application.config['SECRET_KEY'] = os.environ['SECRET_KEY']  
 DBVAR = f"postgresql://{os.environ['RDS_USERNAME']}:{os.environ['RDS_PASSWORD']}@{os.environ['RDS_HOSTNAME']}/{os.environ['RDS_DB_NAME']}"
+DBVAR = 'postgresql://postgres:CarbonApp2025@awseb-e-tmuurzkbwj-stack-awsebrdsdatabase-py5ppvk7vvds.cits0wosai3z.us-east-1.rds.amazonaws.com:5432//ebdb'
 application.config['SQLALCHEMY_DATABASE_URI'] = DBVAR 
 application.config['SQLALCHEMY_BINDS'] ={'transport': DBVAR}
 
